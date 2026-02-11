574332edd15a569cd4d5d9bbcaf20075a7f541f9
Soumyakusuma <soumyakusuma2@gmail.com>
2026-02-11 18:04:33 +0530

    application

---
+++ nodejs/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node App 🚀')
+})
+
+app.get('/health', (req, res) => {
+  res.send('OK')
+})
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000')
+})
